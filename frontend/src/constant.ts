fa4308fdf9fd31a73774ab571710dca62e5bb72f
gil7788 <gil7788@gmail.com>
2025-08-05 18:49:55 +0300

    Implement Frontend and Deloyment Script

---
+++ frontend/src/constant.ts
@@ -0,0 +1,3 @@
+export const PANAGRAM_CONTRACT_ADDRESS = "0x5F858e1cC8e264bBC6D2D70f32DCd28b230F71dd"
+export const ANSWER_HASH = "0x11212d1d1aad94d2dc18aed031902208221aa74484ac3e9122863fba27d5ca36" // This is bytes32(uint256(keccak256("triangles")) % FIELD_MODULUS) but obvs don't put the answer in here lol
+export const ANAGRAM = "GELTSRAIN"
